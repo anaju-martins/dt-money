c06544c9f8b17469d00cf1db8965f3b37d1ec54c
Ana Júlia <aanajulia505@gmail.com>
2025-06-27 22:35:35 -0300

    transactions resume

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,23 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 export function Summary() {
     const { transactions } = useContext(TransactionsContext);
 
+    const summary = transactions.reduce(
+        (acc, transaction) => { 
+            if (transaction.type === 'income') {
+                acc.income += transaction.price;
+                acc.total += transaction.price;
+            } else {
+                acc.outcome += transaction.price;
+                acc.total -= transaction.price;
+            }
+            
+            return acc; 
+        }, 
+        { 
+            income: 0, outcome: 0, total: 0 
+        }
+    )
+    /* reduce permite percorrer um array e reduzir o array a uma nova estrutura de dados, neste caso vai converter um array em um objeto*/
 
     return (
         <SummaryContainer>
@@ -15,7 +32,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color="#00B37E" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -24,7 +41,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
                 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant='green'>
@@ -33,7 +50,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
                 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
 
         </SummaryContainer>
