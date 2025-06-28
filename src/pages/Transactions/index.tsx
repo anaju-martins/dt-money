fdeb90079e9be1d228a66e5f3c94b8fabff2cc8a
Ana Júlia <aanajulia505@gmail.com>
2025-06-27 22:49:17 -0300

    data formatter

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Summary } from '../../components/Summary';
 import { SearchForm } from './components/SearchForm';
 import { PriceHighlight, TransactionContainer, TransactionsTable } from './styles';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { priceFormatter, dateFormatter } from '../../utils/formatter';
 
 export function Transactions(){
 
@@ -23,10 +24,13 @@ export function Transactions(){
                             <tr key={transaction.id}>
                                 <td width="50%">{transaction.description}</td>
                                 <td>
-                                    <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
+                                    <PriceHighlight variant={transaction.type}>
+                                        {transaction.type === 'outcome' && '- '}
+                                        {priceFormatter.format(transaction.price)}
+                                    </PriceHighlight>
                                 </td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>{dateFormatter.format(new Date (transaction.createdAt))}</td>
                             </tr>
                         )
                     })} 
---
+++ src/utils/formatter.ts
@@ -0,0 +1,8 @@
+export const dateFormatter = Intl.DateTimeFormat('pt-BR');
+
+export const priceFormatter = new Intl.NumberFormat('pt-BR',
+    { 
+        style: 'currency', 
+        currency: 'BRL' 
+    }
+);
