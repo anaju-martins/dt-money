5ffe1b557935e8b2b742efa412e4e9b40a86f2cf
Ana Júlia <aanajulia505@gmail.com>
2025-07-10 23:00:53 -0300

    create transaction function

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,9 +10,18 @@ interface Transaction {
     createdAt: string; 
 }
 
+interface CreateTransactionInput {
+    description: string;
+    price: number;
+    category: string;
+    type: 'income' | 'outcome';
+}
+
+
 interface TransactionContextType {
     transactions: Transaction[];
     fetchTransactions: (query?: string) => Promise<void>;
+    createTransaction: (data: CreateTransactionInput) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
@@ -29,10 +38,26 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     async function fetchTransactions(query?: string){
         const response = await api.get('/transactions',{
             params: {
+                _sort: 'createdAt',
+                _order: 'desc',
                 q: query,
             }
         });
         setTransactions(response.data);
+    }
+
+    async function createTransaction (data: CreateTransactionInput ){
+        const { description, price, category, type } = data;
+        
+        const response = await api.post('/transactions', {
+            description,
+            price,
+            category,
+            type,
+            createdAt: new Date(),
+        })
+
+        setTransactions(state => [ response.data, ...state]) /* adiciona a nova transação no array de transações */
     }
     
     useEffect(() => { /* useEffect nao pode ser assincrono */
@@ -40,7 +65,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     }, []) /* quando passa array vazio a função é executada apenas uma vez */
     
     return (
-        <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+        <TransactionsContext.Provider value={{ transactions, fetchTransactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     )
